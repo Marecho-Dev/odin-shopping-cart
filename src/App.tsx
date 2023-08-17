f24fd642afe1444e84682162f577922699922f32
Marek Staszkiewicz <Marekstaszkiewicz@live.com>
2023-08-17 12:19:41 -0400

    added Folder Structure and creating Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { MantineProvider, Text } from "@mantine/core";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
 
 export default function App() {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Text>Welcome to Mantine!</Text>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </MantineProvider>
   );
 }
