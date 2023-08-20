0ac9dd0a0fad4a65f94c64c48ef0fd7d753645f7
Marek Staszkiewicz <Marekstaszkiewicz@live.com>
2023-08-20 11:45:28 -0400

    cart style up

--- src/components/CartDrawer.tsx
+++ src/components/CartDrawer.tsx
@@ -1,9 +1,18 @@
-import { useDisclosure } from "@mantine/hooks";
-import { Drawer, Button, Group } from "@mantine/core";
-
+import { Drawer } from "@mantine/core";
+import { useShoppingCart } from "../context/ShoppingCartContext";
+import { TrashIcon } from "@heroicons/react/24/outline";
+import { Button } from "@mantine/core";
 export function CartDrawer({ onOpen, onClose }) {
   // const [{ open, close }] = useDisclosure(false);
+  const {
+    getItemQuantity,
+    increaseCartQuantity,
+    decreaseCartQuantity,
+    removeFromCart,
+    cartItems,
+  } = useShoppingCart();
 
+  console.log(cartItems);
   return (
     <>
       <Drawer
@@ -17,7 +26,30 @@ export function CartDrawer({ onOpen, onClose }) {
           timingFunction: "linear",
         }}
         style={{ zIndex: "1010", position: "relative" }}
-      ></Drawer>
+      >
+        {cartItems.map((game, index) => (
+          <>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                marginTop: "10px",
+                gap: "10px",
+                width: "100%",
+              }}
+            >
+              <div>{game.id}</div>
+              <div>-</div>
+              <div>{game.quantity}</div>
+              <div>-</div>
+              <div>${game.quantity * 59.99}</div>
+              <Button variant="light" color="red" radius="xs">
+                <TrashIcon style={{ width: "1rem", height: "1rem" }} />
+              </Button>
+            </div>
+          </>
+        ))}
+      </Drawer>
     </>
   );
 }
