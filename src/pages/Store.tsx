3a6eab7866b91304cb642e332b0095947bb3d97f
Marek Staszkiewicz <Marekstaszkiewicz@live.com>
2023-08-20 12:51:49 -0400

    updating store route

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,4 +1,7 @@
-import { Text } from "@mantine/core";
+import { Button, Grid, Text } from "@mantine/core";
+import { GameCard } from "../components/GameCard";
+import { useGames } from "../context/gameContext";
+import { useGenres } from "../context/genresContext";
 export function Store() {
   return <Text>Store</Text>;
 }
